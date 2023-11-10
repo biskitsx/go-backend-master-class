d979ba2ffd3f3d11833f48325f5b4c9217d4645d
biskitsx <112327677+biskitsx@users.noreply.github.com>
2023-11-10 19:50:06 +0700

    add handler of account

---
+++ api/account.go
@@ -0,0 +1,85 @@
+package api
+
+import (
+	"database/sql"
+	"net/http"
+
+	db "github.com/biskitsx/go-backend-master-class/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+type createAccountRequest struct {
+	Owner    string `json:"owner" binding:"required"`
+	Currency string `json:"currency" binding:"required,oneof=USD EUR"`
+}
+
+func (server *Server) createAccount(ctx *gin.Context) {
+	var reqBody createAccountRequest
+	if err := ctx.ShouldBindJSON(&reqBody); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	arg := db.CreateAccountParams{
+		Owner:    reqBody.Owner,
+		Currency: reqBody.Currency,
+		Balance:  0,
+	}
+
+	account, err := server.store.CreateAccount(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, account)
+}
+
+type getAccountRequest struct {
+	ID uint64 `uri:"id" binding:"required,min=1"`
+}
+
+func (server *Server) getAccount(ctx *gin.Context) {
+	var reqUri getAccountRequest
+	if err := ctx.ShouldBindUri(&reqUri); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	account, err := server.store.GetAccount(ctx, int64(reqUri.ID))
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, account)
+}
+
+type listAccountRequest struct {
+	PageID   uint `form:"page_id" binding:"required,min=1"`
+	PageSize uint `form:"page_size" binding:"required,min=1"`
+}
+
+func (server *Server) listAccount(ctx *gin.Context) {
+	var reqQuery listAccountRequest
+	if err := ctx.ShouldBindQuery(&reqQuery); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+	arg := db.ListAccountParams{
+		Limit:  int32(reqQuery.PageSize),
+		Offset: int32(reqQuery.PageID-1) * int32(reqQuery.PageSize),
+	}
+	accounts, err := server.store.ListAccount(ctx, arg)
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, accounts)
+}
