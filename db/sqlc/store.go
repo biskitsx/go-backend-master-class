cbcf1f2b068a9b125fb6e4a46d61cc9b7bcadb43
biskitsx <112327677+biskitsx@users.noreply.github.com>
2023-11-10 17:55:22 +0700

    chapter 8

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -85,24 +85,37 @@ func (store *Store) TranferTx(ctx context.Context, arg TranferTxParams) (Tranfer
 			return err
 		}
 
-		// update account's balance
-		result.FromAccount, err = q.UpdateAccountBalance(ctx, UpdateAccountBalanceParams{
-			ID:     arg.FromAccountID,
-			Amount: -arg.Amount,
-		})
-		if err != nil {
-			return err
-		}
-		result.ToAccount, err = q.UpdateAccountBalance(ctx, UpdateAccountBalanceParams{
-			ID:     arg.ToAccountID,
-			Amount: arg.Amount,
-		})
-		if err != nil {
-			return err
+		// avoid deadlocks by given a order to update accounts
+		if arg.FromAccountID < arg.ToAccountID {
+			result.FromAccount, result.ToAccount, err = addMoney(ctx, q, arg.FromAccountID, -arg.Amount, arg.ToAccountID, arg.Amount)
+			if err != nil {
+				return err
+			}
+		} else {
+			result.ToAccount, result.FromAccount, err = addMoney(ctx, q, arg.ToAccountID, arg.Amount, arg.FromAccountID, -arg.Amount)
+			if err != nil {
+				return err
+			}
+
 		}
 
 		return nil
 	})
 
 	return result, err
+}
+
+func addMoney(ctx context.Context, q *Queries, accountID1 int64, amount1 int64, accountID2 int64, amount2 int64) (account1 Account, account2 Account, err error) {
+	account1, err = q.UpdateAccountBalance(ctx, UpdateAccountBalanceParams{
+		ID:     accountID1,
+		Amount: amount1,
+	})
+	if err != nil {
+		return
+	}
+	account2, err = q.UpdateAccountBalance(ctx, UpdateAccountBalanceParams{
+		ID:     accountID2,
+		Amount: amount2,
+	})
+	return
 }
--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -99,5 +99,53 @@ func TestTranferTx(t *testing.T) {
 	require.Equal(t, updatedAccount2.Balance, account2.Balance+int64(n)*amount)
 
 }
+func TestTranferTxDeadlock(t *testing.T) {
+	store := NewStore(testDB)
+
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+
+	// run n concurrent transfer transactions
+	n := 6
+	amount := int64(10)
+
+	errs := make(chan error)
+	// results := make(chan TranferTxResult)
+	for i := 0; i < n; i++ {
+		fromAccountID := account1.ID
+		toAccountID := account2.ID
+
+		if i%2 == 1 {
+			fromAccountID = account2.ID
+			toAccountID = account1.ID
+		}
+		go func() {
+			_, err := store.TranferTx(context.Background(), TranferTxParams{
+				FromAccountID: fromAccountID,
+				ToAccountID:   toAccountID,
+				Amount:        amount,
+			})
+			errs <- err
+			// results <- result
+		}()
+	}
+
+	// check results
+	for i := 0; i < n; i++ {
+		err := <-errs
+		// result := <-results
+		require.NoError(t, err)
+	}
+	// check the final updated balance
+	updatedAccount1, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+
+	updatedAccount2, err := testQueries.GetAccount(context.Background(), account2.ID)
+	require.NoError(t, err)
+
+	require.Equal(t, updatedAccount1.Balance, account1.Balance)
+	require.Equal(t, updatedAccount2.Balance, account2.Balance)
+
+}
 
 // func createRandomTransfer(t *testing.T, account1, account2 Account) Transfer {}
