4a577393b37b98899de995cc023c6e06358140dd
biskitsx <112327677+biskitsx@users.noreply.github.com>
2023-11-10 19:49:32 +0700

    add database connection

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+
+	_ "github.com/lib/pq"
+
+	"github.com/biskitsx/go-backend-master-class/api"
+	db "github.com/biskitsx/go-backend-master-class/db/sqlc"
+)
+
+const (
+	dbDriver      = "postgres"
+	dbSource      = "postgresql://root:root@localhost:5434/simple_bank?sslmode=disable"
+	serverAddress = "0.0.0.0:8080"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		panic(err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(*store)
+	err = server.Start(serverAddress)
+	if err != nil {
+		panic(err)
+	}
+
 }
