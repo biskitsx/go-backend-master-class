abed8c77dc3648c42a70e3fa8dfa26cc2e92a848
biskitsx <112327677+biskitsx@users.noreply.github.com>
2023-11-11 19:49:42 +0700

    add new binding validator "currency"

--- api/server.go
+++ api/server.go
@@ -3,6 +3,8 @@ package api
 import (
 	db "github.com/biskitsx/go-backend-master-class/db/sqlc"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 )
 
 type Server struct {
@@ -16,9 +18,16 @@ func NewServer(store db.Store) *Server {
 		store:  store,
 		router: router,
 	}
+
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts/", server.listAccount)
+
+	router.POST("/transfers", server.createTransfer)
+
 	return &server
 }
 
