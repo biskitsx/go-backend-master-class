f663482b2e30ee055895220ba24c4cf8245dc785
biskitsx <112327677+biskitsx@users.noreply.github.com>
2023-11-15 19:52:02 +0700

    correct the path of config

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -9,17 +9,12 @@ import (
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:root@localhost:5434/simple_bank?sslmode=disable"
-)
-
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
 	var err error
-	config, err := util.LoadConfig("../.")
+	config, err := util.LoadConfig("../..")
 	if err != nil {
 		panic(err)
 	}
