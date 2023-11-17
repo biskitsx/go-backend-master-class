afb2a16e9790e1f35503f878ecca6edab7bed66a
biskitsx <112327677+biskitsx@users.noreply.github.com>
2023-11-17 21:44:28 +0700

    add config parameter to newServer func+tokenMaker

--- api/server.go
+++ api/server.go
@@ -2,26 +2,42 @@ package api
 
 import (
 	db "github.com/biskitsx/go-backend-master-class/db/sqlc"
+	"github.com/biskitsx/go-backend-master-class/token"
+	"github.com/biskitsx/go-backend-master-class/util"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
 )
 
 type Server struct {
-	store  db.Store
-	router *gin.Engine
+	tokenMaker token.Maker
+	store      db.Store
+	router     *gin.Engine
+	config     util.Config
 }
 
-func NewServer(store db.Store) *Server {
-	router := gin.Default()
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, err
+	}
 	server := Server{
-		store:  store,
-		router: router,
+		store:      store,
+		tokenMaker: tokenMaker,
+		config:     config,
 	}
 
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		v.RegisterValidation("currency", validCurrency)
 	}
+	server.SetupRouter()
+
+	return &server, nil
+}
+
+func (server *Server) SetupRouter() {
+	router := gin.Default()
+
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts/", server.listAccount)
@@ -29,8 +45,8 @@ func NewServer(store db.Store) *Server {
 	router.POST("/transfers", server.createTransfer)
 
 	router.POST("/users", server.createUser)
-
-	return &server
+	router.POST("/users/login", server.loginUser)
+	server.router = router
 }
 
 func (server *Server) Start(address string) error {
