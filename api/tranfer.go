c2301315c020468f421e7a7057a804c87076ac32
biskitsx <112327677+biskitsx@users.noreply.github.com>
2023-11-17 23:45:00 +0700

    authorize user for tranfer

--- api/tranfer.go
+++ api/tranfer.go
@@ -2,10 +2,13 @@ package api
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
+
 	"net/http"
 
 	db "github.com/biskitsx/go-backend-master-class/db/sqlc"
+	"github.com/biskitsx/go-backend-master-class/token"
 	"github.com/gin-gonic/gin"
 )
 
@@ -22,7 +25,23 @@ func (server *Server) createTransfer(ctx *gin.Context) {
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 		return
 	}
-	if !server.validAccount(ctx, reqBody.FromAccountID, reqBody.Currency) || !server.validAccount(ctx, reqBody.ToAccountID, reqBody.Currency) {
+	fromAccount, valid := server.validAccount(ctx, reqBody.FromAccountID, reqBody.Currency)
+	if !valid {
+		err := errors.New("from account doesn't exist")
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload)
+	if fromAccount.Owner != authPayload.Username {
+		err := errors.New("from account doesn't belong to the authenticated user")
+		ctx.JSON(http.StatusUnauthorized, errorResponse(err))
+		return
+	}
+	_, valid = server.validAccount(ctx, reqBody.ToAccountID, reqBody.Currency)
+	if !valid {
+		err := errors.New("from account doesn't exist")
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 		return
 	}
 
@@ -41,70 +60,24 @@ func (server *Server) createTransfer(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, result)
 }
 
-func (server *Server) validAccount(ctx *gin.Context, AccountID int64, currency string) bool {
+func (server *Server) validAccount(ctx *gin.Context, AccountID int64, currency string) (db.Account, bool) {
 	account, err := server.store.GetAccount(ctx, AccountID)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			ctx.JSON(http.StatusNotFound, errorResponse(err))
-			return false
+			return account, false
 		}
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
-		return false
+		return account, false
+
 	}
 
 	if account.Currency != currency {
 		err := fmt.Errorf("account [%d] currency mismatch:", account.ID)
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
-		return false
+		return account, false
+
 	}
-	return true
+	return account, true
+
 }
-
-// type getAccountRequest struct {
-// 	ID uint64 `uri:"id" binding:"required,min=1"`
-// }
-
-// func (server *Server) getAccount(ctx *gin.Context) {
-// 	var reqUri getAccountRequest
-// 	if err := ctx.ShouldBindUri(&reqUri); err != nil {
-// 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
-// 		return
-// 	}
-
-// 	account, err := server.store.GetAccount(ctx, int64(reqUri.ID))
-// 	if err != nil {
-// 		if err == sql.ErrNoRows {
-// 			ctx.JSON(http.StatusNotFound, errorResponse(err))
-// 			return
-// 		}
-// 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
-// 		return
-// 	}
-
-// 	ctx.JSON(http.StatusOK, account)
-// }
-
-// type listAccountRequest struct {
-// 	PageID   uint `form:"page_id" binding:"required,min=1"`
-// 	PageSize uint `form:"page_size" binding:"required,min=1"`
-// }
-
-// func (server *Server) listAccount(ctx *gin.Context) {
-// 	var reqQuery listAccountRequest
-// 	if err := ctx.ShouldBindQuery(&reqQuery); err != nil {
-// 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
-// 		return
-// 	}
-// 	arg := db.ListAccountParams{
-// 		Limit:  int32(reqQuery.PageSize),
-// 		Offset: int32(reqQuery.PageID-1) * int32(reqQuery.PageSize),
-// 	}
-// 	accounts, err := server.store.ListAccount(ctx, arg)
-
-// 	if err != nil {
-// 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
-// 		return
-// 	}
-
-// 	ctx.JSON(http.StatusOK, accounts)
-// }
